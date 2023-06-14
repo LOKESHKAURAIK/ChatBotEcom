aa7e173663ba3714e2df1559461c8d9219d88eed
LOKESHKAURAIK <lokeshbpkauraik@gmail.com>
2023-06-14 08:12:20 +0530

    commit

--- home/views.py
+++ home/views.py
@@ -19,7 +19,6 @@ import openai
 
 
 # os.environ["OPENAI_API_KEY"] = "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"
-os.environ["OPENAI_API_KEY"] = "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"
 
 
 
